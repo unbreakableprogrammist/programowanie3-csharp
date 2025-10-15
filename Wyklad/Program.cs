65ecc200cbac8d3d2688640670ec96ce0808c8db
Krzychulec <criskbp.2019@gmail.com>
2025-10-15 16:01:49 +0200

    stringi

--- Wyklad/Program.cs
+++ Wyklad/Program.cs
@@ -1,7 +1,32 @@
 ﻿using System;
 
-Console.WriteLine("Hello, World!");  // wypisywanie
-string name = Console.ReadLine();   // wczytywanie
-int year = DateTime.Now.Year;
-Console.WriteLine($"Hello {name}! , mamy rok : {year}");
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args[0] == "1")
+            poczatek();
+        else if( args[0] == "2")
+            stringi();
+        
+    }
 
+    static void poczatek()
+    {
+        Console.WriteLine("Hello, World!"); // wypisywanie
+        string name = Console.ReadLine(); // wczytywanie
+        int year = DateTime.Now.Year;
+        Console.WriteLine($"Hello {name}! , mamy rok : {year}");
+    }
+    static void stringi()
+    {
+        string txt = "Hello ";
+        string txt2 = txt + "World";
+        Console.WriteLine(txt2);
+    }
+
+    static void listy()
+    {
+        
+    }
+}
