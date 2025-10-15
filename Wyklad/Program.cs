f68e894c29fadfca9108c33841da7560201e7806
Krzychulec <criskbp.2019@gmail.com>
2025-10-15 15:17:06 +0200

    pierwszy commit

---
+++ Wyklad/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+Console.WriteLine("Hello, World!");  // wypisywanie
+string name = Console.ReadLine();   // wczytywanie
+int year = DateTime.Now.Year;
+Console.WriteLine($"Hello {name}! , mamy rok : {year}");
+
