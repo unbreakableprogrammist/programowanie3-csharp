2181f8fa3aa88b7acea96cdebf2f63e6b06f81da
Krzychulec <criskbp.2019@gmail.com>
2025-10-16 09:30:53 +0200

    typy

--- Wyklad/Program.cs
+++ Wyklad/Program.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿class Osoba
+{
+    public string imie;
+    public string nazwisko;    
+}
 
-class Program
+class Wyklad
 {
     static void Main(string[] args)
     {
@@ -8,7 +12,10 @@ class Program
             poczatek();
         else if( args[0] == "2")
             stringi();
-        
+        else if( args[0] == "3")
+            listy();
+        else if( args[0] == "4")
+            typy();
     }
 
     static void poczatek()
@@ -27,6 +34,62 @@ class Program
 
     static void listy()
     {
+        List<int> numbers = new List<int> { 3,5,12,5,123,512,1,2,12 };
+        numbers.Sort(); // sortowanie listy 
+        numbers.Reverse();  // odwracanie listy
+        numbers.Add(12);  // dodawnaie elementu
+        foreach (var x in numbers)
+            Console.WriteLine(x);
+    }
+
+    static void typy()
+    {
+        /* roznica miedzy typami bezposrednimi ( int char , struct) a referncyjnymi ( string , object , tablice) 
+        jest taka ze objekty bezposrednie sa na stosie , wiec powiedzmy ze w ramie ,a typy refencyjne na stercie
+        i teraz dla kodu :  
+        int x = 10;
+        Osoba osoba = new Osoba();
+        osoba.Imie = "Ala";
+        wyglada to tak :   
+        STOS (Stack)
+        -------------
+        x = 10
+        osoba → (adres #1234)
+
+        STERTA (Heap)
+        -------------
+        #1234 : Osoba
+                Imie = "Ala"
+                
+        wiec w czym roznica , otoz w kopiowaniu 
+        
+        dla bezposrednich typow : 
+        int a = 5;
+        int b = a;
+        b = 10;
+        Console.WriteLine(a); // 5
+        
+        a dlatypow referencyjnych : 
+        Osoba a = new Osoba();
+        Osoba b = a;
+        b.Imie = "Jan";
+        Console.WriteLine(a.Imie); // Jan
+        BO -> b to jest tak naprawde skopiowany adres od osoby a, wiec kopiujemy adres na ktory wskazuje a i jak robimy b.Imie to idziemy do tej samej osoby
+        */
+        int a = 5;
+        int b = a;
+        b = 10;
+        Console.WriteLine(b);
+        Console.WriteLine(a);
+        
+        Osoba goat = new Osoba();
+        goat.imie = "Josue";
+        goat.nazwisko = "Pesqueira";
+        Osoba podroba = goat;
+        podroba.imie = "Pesqueira";
+        Console.WriteLine(goat.imie);
+        Console.WriteLine(goat.nazwisko);
+        
         
     }
 }
--- Wyklad/obj/Debug/net9.0/Wyklad.AssemblyInfo.cs
+++ Wyklad/obj/Debug/net9.0/Wyklad.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wyklad")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d85e5d6718a8a497ca8af967ca8dacab928368d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65ecc200cbac8d3d2688640670ec96ce0808c8db")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wyklad")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Wyklad")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
