4aa93625297b15db6cbd2c07d1bb7102a8c3f9fe
Krzychulec <criskbp.2019@gmail.com>
2025-11-13 19:27:07 +0100

    pliczki

---
+++ filesystem/filesystem/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Console.WriteLine("Working dir:");
+Console.WriteLine(Directory.GetCurrentDirectory());
+
+using FileStream file = File.Open("test.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+Console.WriteLine("OK, file opened/created.");*/
+using FileStream file = File.Open("test.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
