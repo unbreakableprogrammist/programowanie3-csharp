73df6e934c5da17bf86aa8c3e398f7d01c40f02e
Krzychulec <criskbp.2019@gmail.com>
2025-11-13 21:38:07 +0100

    filesystem

--- filesystem/filesystem/Program.cs
+++ filesystem/filesystem/Program.cs
@@ -1,7 +1,7 @@
-﻿using FileStream file = File.Open("test.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+﻿using FileStream file = File.Open("lorem.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
 file.Close(); 
 // czytanie calego tekstu 
-string content = File.ReadAllText("test.txt");
+string content = File.ReadAllText("lorem.txt");
 Console.WriteLine(content);
 
 // tworzy/nadpsuje caly tekst , ktory jest w contencie
@@ -11,6 +11,7 @@ File.WriteAllText("test2.txt", content);
 const string logFile = "log.txt";
 for (int i = 0; i < 100; i++)
 {
+    // append dodaje na koncu pliku 
     File.AppendAllText(logFile, $"[Info] This is a log entry no. {i} \n");
 }
 
@@ -24,4 +25,15 @@ foreach (var line in lines)
 // tworzy/nadpisuje plik lorem_lines , kazdy element tablicy = jedna linia 
 File.WriteAllLines("lorem_lines.txt", lines);
 
+// to samo co ze strintagmi tylko tutaj po prostu bierzemy bity  
+//byte[] bytes = File.ReadAllBytes("lorem.bin");
+//File.WriteAllBytes("lorem.bin.bak", bytes);
 
+//przyklad lazy porgrammingu , bedziemy czytac te linie dopoku gdy zaczniemy isc forem po lines2
+IEnumerable<string> lines2 = File.ReadLines("lorem.txt");
+File.AppendAllLines("lorem.txt.bak", lines2);
+
+foreach (var line in lines2)
+{
+    Console.WriteLine(line);
+}
