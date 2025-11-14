e988a7bb8c6f133a37ef67b817f433beb265a542
Krzychulec <criskbp.2019@gmail.com>
2025-11-14 15:50:26 +0100

    koniec

--- wyklad_filesystem/Download_file_system_tracker/Download_file_system_tracker/Program.cs
+++ wyklad_filesystem/Download_file_system_tracker/Download_file_system_tracker/Program.cs
@@ -1,3 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// bierzemy nowe pliki z downloads o je grupujemy , pliki .txt odczytujemy zawartocs i costam sobie porobimy 
+class Program
+{
+    public static string destinated_path = "/Users/krzys/Desktop/Grouped/";
+    static void Handle_pdfs(string path)
+    {
+        string pdfs = Path.Combine(destinated_path, "pdfs");
+        File.Move(path,pdfs);
+    }
+    static void Handle_moving(string path)
+    {
+        string moving = Path.Combine(destinated_path, "moving");
+        File.Move(path,moving);
+    }
+    static void Handle_code(string path)
+    {
+        string code = Path.Combine(destinated_path, "code");
+        File.Move(path,code);
+    }
+    static void Handle_other(string path)
+    {
+        string other = Path.Combine(destinated_path, "other");
+        File.Move(path,other);
+    }
 
-Console.WriteLine("Hello, World!");
+    static void Handle_txt(string path)
+    {
+        string txt = Path.Combine(destinated_path, "txt");
+        FileStream fs = new FileStream(path,FileMode.OpenOrCreate,FileAccess.ReadWrite);
+        
+        
+    }
+    static void OnCreate(object sender, FileSystemEventArgs e)
+    {
+        switch (Path.GetExtension(e.FullPath).ToLower())
+        {
+            case ".pdf" : 
+                Handle_pdfs(e.FullPath);
+                break;
+            case ".txt" :
+                Handle_txts(e.FullPath);
+                break;
+            case ".mp3":
+            case ".mp4":
+                Handle_moving(e.FullPath);
+                break;
+            case ".c" :
+            case ".cpp" :
+            case ".cs" :
+            case ".py" :
+            case ".js" :
+                Handle_code(e.FullPath);
+                break;
+            default:
+                Handle_other(e.FullPath);
+                break;
+        }
+    }
+    public static void Watch(string from_path)
+    {
+        using var watcher = new FileSystemWatcher(from_path,"*");
+        watcher.NotifyFilter = NotifyFilters.FileName;  // sledzimy created , deleted , renamed
+        watcher.Created += OnCreate;
+        
+    }
+    static void Main(string[] args)
+    {
+        string download_path = "/Users/krzys/Downloads/";
+        Watch(download_path);
+    }
+}
